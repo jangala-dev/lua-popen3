671e1b6d9b9374470881ae6a3760fa9253731a4e
TsT <tst2005@gmail.com>
2015-09-01 21:35:01 +0200

    split popen3 and pipe stuff

--- pipe.lua
+++ pipe.lua
@@ -15,40 +15,7 @@
 
 local posix = require("posix")
  
---
--- Simple popen3() implementation
---
-local function popen3(path, ...)
-	local r0, w0 = posix.pipe()
-	local r1, w1 = posix.pipe()
-	local r2, w2 = posix.pipe()
-
-	assert((w0 ~= nil and r1 ~= nil and r2 ~= nil), "pipe() failed")
-
-	local pid, err = posix.fork()
-	assert(pid ~= nil, "fork() failed")
-	if pid == 0 then
-		posix.close(w0)
-		posix.close(r1)
-		posix.close(r2)
-
-		posix.dup2(r0, posix.fileno(io.stdin))
-		posix.dup2(w1, posix.fileno(io.stdout))
-		posix.dup2(w2, posix.fileno(io.stderr))
-
-		local ret, err = posix.execp(path, ...)
-		assert(ret ~= nil, "execp() failed")
-
-		posix._exit(1)
-		return
-	end
-
-	posix.close(r0)
-	posix.close(w1)
-	posix.close(w2)
-
-	return pid, w0, r1, r2
-end
+local popen3 = require "popen3"
 
 --
 -- Pipe an array of input into the same cmd + optional arguments (each
